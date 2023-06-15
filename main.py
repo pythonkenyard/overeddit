2e95384f609a8ddf0aedf828e5e85d790096aa65
pythonkenyard <77246641+pythonkenyard@users.noreply.github.com>
2023-06-16 00:14:52 +0100

    Update main.py
    Finalised script with a backup option when api fails to ensure comment deletion

--- main.py
+++ main.py
@@ -6,23 +6,25 @@ import csv
 #--------------UPDATE THESE FIELDS---------------------------------------------------
 
 #COMPULSORY
-bearer = ""                                     #this will be a long field starting 'eyJ....' its basically your cookie
-
+bearer = ""                #this will be a long field starting 'eyJ....' its basically your cookie
+id = ""                    # this may not be  compulsory or can be any value? its used for backup when the api fails to ensure deletion of comments. (maybe 1 in every 15 fails if not used)
 
 #OPTIONAL
 comment = "deleted comment due to api change"   # edit for your custom comment
-delay = 1.5                                       # seconds between comments. Reddit has specified oauth should be able to to 60-100 requests per minute
 csv_file = ""                                   #link to csv file. will be prompted if not included.
+
+delay = 1.5                                       # seconds between comments. Reddit has specified oauth should be able to to 60-100 requests per minute
 
 #-----------------SCRIPT DONT CHANGE BELOW THIS LINE--------------------------------------------------------------------------
 running = False
 comment = comment.replace(" ","%20") # needed format in the post request.
 
 def prompt_selection():
-  #os.system("cls") # clear the cmd line
+  os.system("cls") # clear the cmd line
+  print("##OVEREDDIT##\nVer 1.0\nurl: https://github.com/pythonkenyard/overeddit\n\n")
   print("(1) overwrite comments:\
         \n(z) Exit")
-  selection = input("choose:")
+  selection = input("Input num selection:")
   if selection == "1":
     return False, "overwrite"
 
@@ -30,11 +32,18 @@ def prompt_selection():
     return True, "skip"
 
   else:
-    print("unknown input. please input e.g. 1 or z")
+    print("WARNING unknown input. please input e.g. 1 or z")
+    print("resuming in 3")
+    time.sleep(1)
+    print("2")
+    time.sleep(1)
+    print("1")
+    time.sleep(1)
     return False, "skip"
 
 #MAIN FUNCTION FOR OVERWRITING. IT IS JUST A POST REQUEST AS PER CHROME
 def overwrite(item,num,duration):
+  global comment
   headers = {
       'authority': 'oauth.reddit.com',
       'accept': '*/*',
@@ -69,8 +78,53 @@ def overwrite(item,num,duration):
     print("SUCCESS")
     return "SUCCESS"
   else:
-    print("ERROR")
-    return "ERROR"
+    print(f"ERROR, trying backup option not via api")
+    time.sleep(1)
+    headers = {
+    'authority': 'gql.reddit.com',
+    'accept': '*/*',
+    'accept-language': 'en-US,en;q=0.9,es;q=0.8,de-DE;q=0.7,de;q=0.6,it;q=0.5',
+    'authorization': f'Bearer {bearer}',
+    'content-type': 'application/json',
+    'dnt': '1',
+    'origin': 'https://www.reddit.com',
+    'referer': 'https://www.reddit.com/',
+    'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+    'x-reddit-compression': '1',
+}
+    comment = comment.replace("%20"," ")
+    json_data = {
+    'id': f'{id}',
+    'variables': {
+        'input': {
+            'commentId': f't1_{item}',
+            'content': {
+                'markdown': f'{comment} {num} of {duration}',
+                'richText': None,
+            },
+        },
+    },
+}
+
+    response = requests.post('https://gql.reddit.com/', headers=headers, json=json_data)
+    if response.status_code == 200:
+        print("SUCCESS on second try")
+        return "SUCCESS"
+    else:
+        print(f"Final ERROR, NOTE YOU THERE IS AN ISSUE LIKELY WITH YOUR BEARER OR ID.")
+        time.sleep(4)
+        if response.status_code == 200:
+            print("SUCCESS on final try")
+            return "SUCCESS"
+        else:
+            print("FAILED")
+            return "ERROR"
   
 # load the csv file and return everything from column 1
 def load_file(csvfile):
@@ -146,3 +200,4 @@ if __name__ == "__main__":
         
         elif action == "delete":
             print("no support for deleting yet.")
+      
